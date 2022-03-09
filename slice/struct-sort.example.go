f4f365731b2ff4dbc3d3aa4455517b408823e62d
bbaktaeho <lth9036@gmail.com>
2022-03-10 00:41:21 +0900

    add the sorting unsupported sort.Interface struct

--- slice/struct-sort.example.go
+++ slice/struct-sort.example.go
@@ -10,14 +10,21 @@ type A struct {
 	b int
 }
 
+type B struct {
+	a string
+	b int
+}
+
 type As []A
 
-// sort.Interface 타입으로 맞춤
 func (as As) Len() int           { return len(as) }
 func (as As) Less(i, j int) bool { return as[i].b < as[j].b }
 func (as As) Swap(i, j int)      { as[i], as[j] = as[j], as[i] }
 
-func main() {
+// sort.Interface 타입을 지원하는 구조체 정렬 예제
+func ExampleSortInterfaceSort() {
+	fmt.Println("start ExampleSortInterfaceSort()")
+	defer fmt.Println("end ExampleSortInterfaceSort()")
 	as := []A{
 		{"aa", 5},
 		{"bb", 9},
@@ -27,4 +34,24 @@ func main() {
 
 	sort.Sort(As(as))
 	fmt.Println(as)
+}
+
+// sort.Interface 타입을 지원하지 않는 구조체 정렬 예제
+func ExampleDefaultStructSort() {
+	fmt.Println("start ExampleDefaultStructSort()")
+	defer fmt.Println("end ExampleDefaultStructSort()")
+	bs := []B{
+		{"aa", 5},
+		{"bb", 9},
+		{"cc", 1},
+		{"dd", 8},
+	}
+
+	sort.Slice(bs, func(i, j int) bool { return bs[i].b < bs[j].b })
+	fmt.Println(bs)
+}
+
+func main() {
+	ExampleDefaultStructSort()
+	ExampleSortInterfaceSort()
 }
